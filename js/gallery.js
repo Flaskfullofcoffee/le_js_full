2d7af77d79b884bcb16328bdf64532e49ad614bb
Zac <zdobbs3@gmail.com>
2019-02-09 12:37:14 -0700

    created event listener function

---
+++ js/gallery.js
@@ -0,0 +1,14 @@
+// attach all img thumbnails to a variable.
+function activateGallery() {
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+    });
+  });
+}
